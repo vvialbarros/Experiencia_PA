ab67d98e92577c9b2ea46ccc1d39ce0423f6e091
benjahuenchunir <bahuenchunir@uc.cl>
2024-03-09 18:34:37 -0300

    first commit

---
+++ desencriptar.py
@@ -0,0 +1,22 @@
+
+
+def desencriptar_mensaje(mensaje_encriptado: str) -> str:
+    """
+    Esta función toma un mensaje encriptado como entrada y lo desencripta utilizando un libro_super_secreto.txt. 
+    Cada letra del mensaje original viene codificada como string con la siguiente estructura:
+    
+    "numero_linea,numero_palabra,numero_letra_en_palabra"
+    
+    Donde:
+    - numero_linea es la línea en la que se encuentra la letra en el libro.
+    - numero_palabra es la palabra en la que se encuentra la letra en la línea.
+    - numero_letra_en_palabra es la posición de la letra en la palabra.
+    
+    Luego se debe buscar en el libro la letra correspondiente a cada string y concatenar todas las letras para formar el mensaje original.
+    
+    Recibe:
+        mensaje_encriptado (str): El mensaje que se va a desencriptar.
+        
+    Retorna:
+        str: El mensaje desencriptado.
+    """
---
+++ encriptar.py
@@ -0,0 +1,21 @@
+
+def encriptar_mensaje(mensaje: str) -> str:
+    """
+    Esta función toma un mensaje como entrada y lo encripta utilizando un libro_super_secreto.txt. 
+    Cada letra del mensaje se busca en el libro y se transforma en un string con la siguiente estructura:
+    
+    "numero_linea,numero_palabra,numero_letra_en_palabra"
+    
+    Donde:
+    - numero_linea es la línea en la que se encuentra la letra en el libro.
+    - numero_palabra es la palabra en la que se encuentra la letra en la línea.
+    - numero_letra_en_palabra es la posición de la letra en la palabra.
+    
+    Luego se concatenan todos los strings de cada letra para formar el mensaje encriptado.
+    
+    Recibe:
+        mensaje (str): El mensaje que se va a encriptar.
+        
+    Retorna:
+        str: El mensaje encriptado.
+    """
