f0984d039d9650cdd1509167469fab288a1a615c
benjahuenchunir <bahuenchunir@uc.cl>
2024-03-11 16:00:17 -0300

    Added example to docstrings

--- desencriptar.py
+++ desencriptar.py
@@ -14,6 +14,15 @@ def desencriptar_mensaje(mensaje_encriptado: str) -> str:
     
     Luego se debe buscar en el libro la letra correspondiente a cada string y concatenar todas las letras para formar el mensaje original.
     Los espacios del mensaje original se representan con un punto y coma (;).
+    Los numeros parten desde 0.
+    
+    Por ejemplo, la frase codificada 12,0,0,0,1,11,0,11,6;0,1,2,0,1,3,0,1,5,0,1,11,0,11,6;0,2,0,0,1,11,0,1,7,0,3,1,0,1,9,0,1,3,0,1,4,3,6,6,0,1,9;0,1,1,0,0,1;0,1,7,0,6,4,0,1,9,0,2,0,0,1,11
+    se desencripta como:
+    Linea 12, palabra 0, letra 0 -> L
+    Linea 0, palabra 1, letra 11 -> o
+    Linea 0, palabra 11, letra 6 -> s
+    ; -> espacio
+    Siguiendo se obtiene el mensaje original "Los patos dominarán el mundo" 
     
     Recibe:
         mensaje_encriptado (str): El mensaje que se va a desencriptar.
--- encriptar.py
+++ encriptar.py
@@ -13,6 +13,14 @@ def encriptar_mensaje(mensaje: str) -> str:
     
     Luego se concatenan todos los strings de cada letra para formar el mensaje encriptado.
     Los espacios entre palabras se representan con un punto y coma (;).
+    Los numeros parten desde 0.
+    
+    Por ejemplo, la frase "Los patos dominarán el mundo" se encripta como:
+    12,0,0,0,1,11,0,11,6;0,1,2,0,1,3,0,1,5,0,1,11,0,11,6;0,2,0,0,1,11,0,1,7,0,3,1,0,1,9,0,1,3,0,1,4,3,6,6,0,1,9;0,1,1,0,0,1;0,1,7,0,6,4,0,1,9,0,2,0,0,1,11
+    Linea 12, palabra 0, letra 0 -> L
+    Linea 0, palabra 1, letra 11 -> o
+    Linea 0, palabra 11, letra 6 -> s
+    ; -> espacio
     
     Recibe:
         mensaje (str): El mensaje que se va a encriptar.
