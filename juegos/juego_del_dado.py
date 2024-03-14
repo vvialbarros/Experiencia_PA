afa3f23d30c8845c457daee7dedc5b432c5779af
vvialbarros <163348885+vvialbarros@users.noreply.github.com>
2024-03-14 13:47:15 +0000

    Termine el juego del dado

--- juegos/juego_del_dado.py
+++ juegos/juego_del_dado.py
@@ -1,8 +1,27 @@
+import random
+
 def juego_del_dado():
+    puntaje_jugador=0
+    puntaje_compu=0
+    input('Presione la tecla Enter')
+
+    while puntaje_jugador<30 and puntaje_compu<30:
+        puntaje_jugador+=random.randint(1,6)
+        puntaje_compu+=random.randint(1,6)
+
+    if puntaje_jugador>=30 and puntaje_compu>=30:
+        print('Empate')
+    elif puntaje_jugador>=30 and puntaje_compu<30:
+        print('Gano el jugador')
+    elif puntaje_compu>=30 and puntaje_jugador<30:
+        print('Gano el computador')
+
+
+
     """
     Esta función tiene que pedirle al usuario que aprete enter para que lance un dado.
     Esto genera un número al azar que se le suma a la puntuación del usuario.
     Después el computador también tiene que lanzar un dado.
     El primero en sumar 30 puntos gana.
     """
-    pass
+
