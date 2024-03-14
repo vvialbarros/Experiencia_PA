07012c30d5890e2d6232c456b6962ed9f8470cb4
jx4jordan <155557075+jx4jordan@users.noreply.github.com>
2024-03-14 15:57:28 +0000

    arreglo par impar

--- juegos/adivinar_par_o_impar.py
+++ juegos/adivinar_par_o_impar.py
@@ -1,13 +1,19 @@
 import random
 def adivinar_par_o_impar():
     numero_pc = random.randint(1,2)
-    guess = input('Adivina si el numero sera par o impar')
+    guess = input('Adivina si el numero sera par o impar \n')
+    poi = ''
+
+    if numero_pc == 1:
+        poi = 'impar'
+    elif numero_pc == 2:
+        poi = 'par'
 
     if guess == 'par' and numero_pc == 2:
         print('Adivinaste, es par!')
     elif guess == 'impar' and numero_pc == 1:
         print('Adivinaste, es impar!')
     else:
-        print('No adivinaste.')
+        print(f'No adivinaste, era {poi}.')
 
     pass
